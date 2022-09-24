6c232ea548fa56fc40ee5dc5217a931969fb387d
Adarsh78513 <adrsrj@gmail.com>
2022-09-24 23:24:50 +0530

    Before makign index

--- src/cpmponents/Header.js
+++ src/cpmponents/Header.js
@@ -1,12 +1,8 @@
 const Header = () => {
     return (
         <header className="header">
-            <div className="lab-name-header">
-                <span style={{color:"red"}}>HiPeC Lab</span>
-            </div>
-            <div className="header-discription">
-                <span className="header-hipec-expantion"><span style={{color:"red"}}>Hi</span>gh <span style={{color:"red"}}>Pe</span>rformance <span style={{color:"red"}}>C</span>omputing <span style={{color:"red"}}>Lab</span> IIITD </span>
-            </div>
+                <div className="lab-name-header"><span style={{color:"red"}}>HiPeC Lab</span></div>
+                <div className="header-discription"><span style={{color:"red"}}>Hi</span>gh <span style={{color:"red"}}>Pe</span>rformance <span style={{color:"red"}}>C</span>omputing <span style={{color:"red"}}>Lab</span> IIITD</div>
         </header>
     );
 }
