51f0d8c74f37ac1b8a7b6a315facfe300964e1ad
Adarsh78513 <adrsrj@gmail.com>
2022-12-10 08:31:53 +0530

    new start

--- src/cpmponents/LinkButton.js
+++
@@ -1,9 +0,0 @@
-const LinkButton = ({link, text}) => {
-    return (
-        <button className="link-button">
-            <a href={link}>{text}</a>
-        </button>
-    );
-}
-
-export default LinkButton;
